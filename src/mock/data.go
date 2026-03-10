e16c99e89868365ef96be9a7fb884426fb128a3c
liuyanzheng <liuyanzheng@didiglobal.com>
2026-03-10 15:57:10 +0800

    班次召回逻辑

--- src/mock/data.go
+++ src/mock/data.go
@@ -147,6 +147,136 @@ func LoadMockSchedules() []models.Schedule {
 				},
 			},
 		},
+		// 添加更多班次数据
+		{
+			ID:             "s6",
+			City:           "北京",
+			TotalSeats:     55,
+			RemainingSeats: 25,
+			DepartureTime:  baseTime.Add(5 * time.Hour),
+			Stations: []models.Station{
+				{
+					Name:          "北京西站",
+					ArrivalTime:   baseTime.Add(5 * time.Hour),
+					DepartureTime: baseTime.Add(5*time.Hour + 5*time.Minute),
+					Lat:           39.8942,
+					Lng:           116.3229,
+				},
+				{
+					Name:          "太原站",
+					ArrivalTime:   baseTime.Add(7*time.Hour + 30*time.Minute),
+					DepartureTime: baseTime.Add(7*time.Hour + 35*time.Minute),
+					Lat:           37.8706,
+					Lng:           112.5624,
+				},
+			},
+		},
+		{
+			ID:             "s7",
+			City:           "上海",
+			TotalSeats:     45,
+			RemainingSeats: 10,
+			DepartureTime:  baseTime.Add(3 * time.Hour),
+			Stations: []models.Station{
+				{
+					Name:          "上海南站",
+					ArrivalTime:   baseTime.Add(3 * time.Hour),
+					DepartureTime: baseTime.Add(3*time.Hour + 5*time.Minute),
+					Lat:           31.1944,
+					Lng:           121.4379,
+				},
+				{
+					Name:          "南京站",
+					ArrivalTime:   baseTime.Add(4*time.Hour + 40*time.Minute),
+					DepartureTime: baseTime.Add(4*time.Hour + 45*time.Minute),
+					Lat:           32.0584,
+					Lng:           118.7965,
+				},
+				{
+					Name:          "徐州站",
+					ArrivalTime:   baseTime.Add(6*time.Hour + 20*time.Minute),
+					DepartureTime: baseTime.Add(6*time.Hour + 25*time.Minute),
+					Lat:           34.2658,
+					Lng:           117.1859,
+				},
+			},
+		},
+		{
+			ID:             "s8",
+			City:           "广州",
+			TotalSeats:     60,
+			RemainingSeats: 35,
+			DepartureTime:  baseTime.Add(2 * time.Hour),
+			Stations: []models.Station{
+				{
+					Name:          "广州站",
+					ArrivalTime:   baseTime.Add(2 * time.Hour),
+					DepartureTime: baseTime.Add(2*time.Hour + 5*time.Minute),
+					Lat:           23.1549,
+					Lng:           113.2644,
+				},
+				{
+					Name:          "深圳站",
+					ArrivalTime:   baseTime.Add(3*time.Hour + 10*time.Minute),
+					DepartureTime: baseTime.Add(3*time.Hour + 15*time.Minute),
+					Lat:           22.5431,
+					Lng:           114.0579,
+				},
+			},
+		},
+		{
+			ID:             "s9",
+			City:           "北京",
+			TotalSeats:     40,
+			RemainingSeats: 15,
+			DepartureTime:  baseTime.Add(6 * time.Hour),
+			Stations: []models.Station{
+				{
+					Name:          "北京北站",
+					ArrivalTime:   baseTime.Add(6 * time.Hour),
+					DepartureTime: baseTime.Add(6*time.Hour + 5*time.Minute),
+					Lat:           39.9444,
+					Lng:           116.3583,
+				},
+				{
+					Name:          "张家口站",
+					ArrivalTime:   baseTime.Add(7*time.Hour + 40*time.Minute),
+					DepartureTime: baseTime.Add(7*time.Hour + 45*time.Minute),
+					Lat:           40.8106,
+					Lng:           114.8755,
+				},
+			},
+		},
+		{
+			ID:             "s10",
+			City:           "上海",
+			TotalSeats:     50,
+			RemainingSeats: 20,
+			DepartureTime:  baseTime.Add(4 * time.Hour),
+			Stations: []models.Station{
+				{
+					Name:          "上海虹桥站",
+					ArrivalTime:   baseTime.Add(4 * time.Hour),
+					DepartureTime: baseTime.Add(4*time.Hour + 5*time.Minute),
+					Lat:           31.1978,
+					Lng:           121.3356,
+				},
+				{
+					Name:          "苏州站",
+					ArrivalTime:   baseTime.Add(4*time.Hour + 30*time.Minute),
+					DepartureTime: baseTime.Add(4*time.Hour + 35*time.Minute),
+					Lat:           31.2989,
+					Lng:           120.5853,
+				},
+				{
+					Name:          "无锡站",
+					ArrivalTime:   baseTime.Add(4*time.Hour + 50*time.Minute),
+					DepartureTime: baseTime.Add(4*time.Hour + 55*time.Minute),
+					Lat:           31.5593,
+					Lng:           120.3026,
+				},
+			},
+		},
 	}
 
 	return schedules
@@ -164,5 +294,68 @@ func LoadMockUserRequest() models.UserRequest {
 		DropoffLng:    117.2009,
 		Page:          1,
 		PageSize:      10,
+	}
+}
+
+// LoadMockUserRequests 加载多个Mock用户请求数据
+func LoadMockUserRequests() []models.UserRequest {
+	baseTime := time.Now()
+
+	return []models.UserRequest{
+		{
+			City:          "北京",
+			SeatCount:     2,
+			DepartureTime: baseTime.Add(2 * time.Hour),
+			PickupLat:     39.9042,
+			PickupLng:     116.4074,
+			DropoffLat:    39.0842,
+			DropoffLng:    117.2009,
+			Page:          1,
+			PageSize:      10,
+		},
+		{
+			City:          "上海",
+			SeatCount:     3,
+			DepartureTime: baseTime.Add(3 * time.Hour),
+			PickupLat:     31.2304,
+			PickupLng:     121.4737,
+			DropoffLat:    30.2741,
+			DropoffLng:    120.1551,
+			Page:          1,
+			PageSize:      10,
+		},
+		{
+			City:          "北京",
+			SeatCount:     1,
+			DepartureTime: baseTime.Add(4 * time.Hour),
+			PickupLat:     39.8652,
+			PickupLng:     116.3786,
+			DropoffLat:    36.6683,
+			DropoffLng:    117.0207,
+			Page:          1,
+			PageSize:      10,
+		},
+		{
+			City:          "广州",
+			SeatCount:     4,
+			DepartureTime: baseTime.Add(2 * time.Hour),
+			PickupLat:     23.1549,
+			PickupLng:     113.2644,
+			DropoffLat:    22.5431,
+			DropoffLng:    114.0579,
+			Page:          1,
+			PageSize:      10,
+		},
+		{
+			City:          "北京",
+			SeatCount:     2,
+			DepartureTime: baseTime.Add(5 * time.Hour),
+			PickupLat:     39.8942,
+			PickupLng:     116.3229,
+			DropoffLat:    38.0452,
+			DropoffLng:    114.5149,
+			Page:          1,
+			PageSize:      10,
+		},
 	}
 }
--- src/start/main.go
+++ src/start/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"sync"
+	"time"
 
 	greeting "temporal-exp/src/greating/workflow"
-	"temporal-exp/src/mock"
+	mock "temporal-exp/src/mock"
+	"temporal-exp/src/models"
 	schedule "temporal-exp/src/schedule/workflow"
 
 	"go.temporal.io/sdk/client"
@@ -23,6 +27,9 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "schedule" {
 		// 运行班次推荐工作流
 		runScheduleWorkflow(c)
+	} else if len(os.Args) > 1 && os.Args[1] == "batch" {
+		// 批量运行班次推荐工作流
+		runBatchScheduleWorkflows(c)
 	} else {
 		// 运行问候工作流
 		runGreetingWorkflow(c)
@@ -75,4 +82,60 @@ func runScheduleWorkflow(c client.Client) {
 		log.Fatalln("Unable get workflow result", err)
 	}
 	log.Println("Workflow result:", result)
+}
+
+func runBatchScheduleWorkflows(c client.Client) {
+	// 加载多个Mock用户请求
+	requests := mock.LoadMockUserRequests()
+
+	// 创建等待组
+	var wg sync.WaitGroup
+
+	// 执行所有请求
+	for i, request := range requests {
+		// 添加到等待组
+		wg.Add(1)
+
+		// 启动goroutine执行工作流
+		go func(idx int, req models.UserRequest) {
+			defer wg.Done()
+
+			// 生成唯一的工作流ID
+			workflowID := fmt.Sprintf("schedule-recommendation-workflow-%d", idx)
+
+			options := client.StartWorkflowOptions{
+				ID:        workflowID,
+				TaskQueue: "my-task-queue",
+			}
+
+			we, err := c.ExecuteWorkflow(context.Background(), options, schedule.RecommendSchedulesWorkflow, req)
+			if err != nil {
+				log.Printf("Unable to execute workflow %d: %v", idx, err)
+				return
+			}
+			log.Printf("Started workflow %d, WorkflowID: %s, RunID: %s", idx, we.GetID(), we.GetRunID())
+
+			var result interface{}
+			err = we.Get(context.Background(), &result)
+			if err != nil {
+				log.Printf("Unable get workflow %d result: %v", idx, err)
+				return
+			}
+			log.Printf("Workflow %d result: %v", idx, result)
+		}(i, request)
+
+		// 每间隔短暂时间执行一个请求，模拟真实的线上情况
+		// 为了模拟并行情况，前两个请求间隔时间较短
+		if i == 0 {
+			time.Sleep(100 * time.Millisecond)
+		} else if i == 1 {
+			time.Sleep(50 * time.Millisecond)
+		} else {
+			time.Sleep(200 * time.Millisecond)
+		}
+	}
+
+	// 等待所有工作流执行完成
+	wg.Wait()
+	log.Println("All workflow executions completed")
 }
